46c3b60c7d574cfff9d91aa15b60eb5f3e98d1a1
Stanislav <sktytsiuks@gmail.com>
2026-04-07 18:56:09 +0200

    correct folder for rag

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ project_root = Path(__file__).parent.parent
 input_dir = str(project_root / "data" / "input_files")
 output_dir = str(project_root / "data" / "cleaned_files")
 
-cleaned_files = clean_all_pdfs(input_dir,output_dir)
+#cleaned_files = clean_all_pdfs(input_dir,output_dir)
 # Initialize
-rag = SimpleRAG("data/cleaned_files", model="llama2")
+rag = SimpleRAG(output_dir, model="llama2")
 
 # Ask questions
 answer = rag.ask("What is this document about?")
